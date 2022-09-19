936e1bca8bc2ca5df09d90e962592f3756cefcc6
Jefferson Barbosa <jbs@cin.ufpe.br>
2022-09-19 03:57:53 -0300

    atualizar actions

--- teste/test_calculator.py
+++ teste/test_calculator.py
@@ -8,38 +8,31 @@ class TestCalculator():
     def setup_class(self):
         self.calc = Calculator()
 
+ 
 
-    #def setup_method(self):
-     #   self.calc = Calculator()
-    
-
-    def teardown_class(self):
-        print("!!")
+    def teardown_method(self):
+        print(" fim do test")
 
 
-#    def teardown_method(self):
-#        print("!!")
-
-
+    @pytest.mark.skip(reason=" funcionadidade ainda não implementada") 
     def test_addition(self):
        
         assert self.calc.addition(2,2) == 4
     
 
-    @pytest.mark.skip(reason="caso de teste duplicado") #notação para pulao o teste
+    @pytest.mark.skip(reason=" funcionadidade ainda não implementada") 
     def test_subtraction(self):
        
-        assert self.calc.subtraction(10,1) == 9, "Esperava 3, mas o resultado foi {}".format(self.calc.subtraction(10,1))
+          assert self.calc.subtraction(10,1) == 9, "Esperava 3, mas o resultado foi {}".format(self.calc.subtraction(10,1))
     
+    @pytest.mark.skip(reason=" funcionadidade ainda não implementada") 
     def test_multiplication(self):
         
         assert self.calc.multiplication(3,4) == 12
-    
+
+    def test_additionDois(self):
+        assert self.calc.addition(6,6) == 12
+
     def test_division(self):
        
-        assert self.calc.division(10,2) == 5
-
-    def test_division_by_zero(self):
-        
-        with pytest.raises(ZeroDivisionError):
-            self.calc.division(10,0)
+      assert self.calc.division(10,2) == 5
